9595f8becaef3cb0af33273b9a14070974a26d93
WMYeah_Mac <george012@163.com>
2024-05-14 16:24:33 +0800

    Update v0.0.14

--- config/config.go
+++ config/config.go
@@ -5,6 +5,6 @@ package config
 
 const (
 	ProjectName        = "blockchain_box"
-	ProjectVersion     = "v0.0.13"
+	ProjectVersion     = "v0.0.14"
 	ProjectDescription = "[blockchain] box"
 )
--- service_info/service_info.go
+++ service_info/service_info.go
@@ -92,9 +92,10 @@ func GetServiceFlagNumberForStr(str string) ServiceFlag {
 }
 
 type ServiceInfoBase struct {
-	ServiceFlagNone        ServiceFlag                      // 服务标识：自定义 or 第三方标准服务
+	ServiceFlag            ServiceFlag                      // 服务标识：自定义 or 第三方标准服务
 	ServiceStatus          ServiceStatus                    // 服务运行状态
 	ServiceStaredTime      time.Time                        // 启动时间
+	ServiceName            string                           // 服务名
 	ServiceRemark          string                           // 服务备注名
 	ServiceRunPath         string                           // 服务程序路径
 	ServiceDataPath        string                           // 服务数据文件路径
