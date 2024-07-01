612ca7165b0726e42a52d1f1dec6e1d686ef00a7
WMYeah_Mac <george017@163.com>
2024-07-01 21:07:44 +0800

    Update v0.0.24

--- config/config.go
+++ config/config.go
@@ -5,6 +5,6 @@ package config
 
 const (
 	ProjectName        = "blockchain_box"
-	ProjectVersion     = "v0.0.23"
+	ProjectVersion     = "v0.0.24"
 	ProjectDescription = "blockchain develop tool box"
 )
