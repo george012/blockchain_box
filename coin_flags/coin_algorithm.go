cdf6b54fe21d7da5805976ae44ae0bfb97bc99af
WMYeah_Mac <george017@163.com>
2024-07-27 15:06:28 +0800

    update coin Pow Algorithm

--- coin_flags/coin_algorithm.go
+++ coin_flags/coin_algorithm.go
@@ -1,6 +1,12 @@
 package coin_flags
 
-func (cf CoinFlag) Algorithm() string {
+// PowAlgorithm
+/*
+	en: get pow Algorithm;
+	zh-CN: 获取coin 对应的 Pow算法;
+	@return [☑][type: string] en: Algorithm string ;zh-CN: 算法字符串;
+*/
+func (cf CoinFlag) PowAlgorithm() string {
 	switch cf {
 	case CoinFlagBTC:
 		return "SHA256d"
@@ -9,7 +15,27 @@ func (cf CoinFlag) Algorithm() string {
 	case CoinFlagDOGE:
 		return "Scrypt"
 	case CoinFlagETC:
-		return "ETCHash"
+		return "EtcHash"
+	case CoinFlagETHW:
+		return "Ethash"
+	case CoinFlagZIL:
+		return "Ethash"
+	case CoinFlagOCTA:
+		return "Ethash"
+	case CoinFlagMETA:
+		return "Ethash"
+	case CoinFlagCAU:
+		return "Ethash"
+	case CoinFlagDNX:
+		return "DynexSolve"
+	case CoinFlagETH:
+		return "none"
+	case CoinFlagSOL:
+		return "none"
+	case CoinFlagFIL:
+		return "none"
+	case CoinFlagAleo:
+		return "zkSNARK"
 	default:
 		return "none"
 	}
