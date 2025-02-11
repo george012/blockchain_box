14505d185e9560459122a7184cf3e8080964ebe1
M3 <george017@163.com>
2025-02-11 17:02:04 +0800

    Update v0.0.37

--- config/config.go
+++ config/config.go
@@ -5,6 +5,6 @@ package config
 
 const (
 	ProjectName        = "blockchain_box"
-	ProjectVersion     = "v0.0.36"
+	ProjectVersion     = "v0.0.37"
 	ProjectDescription = "blockchain develop tool box"
 )
